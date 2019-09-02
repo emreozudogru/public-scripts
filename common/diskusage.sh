7d72a4098662f153180c2ff540c027d86167e45f
Emre Ozudogru <emre@ozudogru.com>
2019-09-02 11:44:14 +0300

    Update

--- common/diskusage.sh
+++ common/diskusage.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# 
+# update code 	
+# wget --backups=1 https://gitlab.com/frmax/public-scripts/raw/master/common/diskusage.sh -O /usr/local/bin/diskusage.sh
+#
 
 #admin email hesabi
 ADMIN="sistem@turknet.net.tr"
@@ -7,7 +11,8 @@ ADMIN="sistem@turknet.net.tr"
 SUPPORT="noc@turknet.net.tr"
 
 #belirlenen threshold degeri
-THRESHOLD=95
+THRESHOLDPERCENT=90
+THRESHOLDBYTE=104857600‬
 
 #hostname
 HOSTNAME=$(hostname)
@@ -18,20 +23,24 @@ MAIL=/bin/mail
 # tum disk bilgileri buraya depolaniyor
 EMAIL=""
 
-for line in $(df -hP | egrep '^/dev/' | awk '{ print $6 "_:_" $5 }')
+for line in $(df -hP | egrep '^/dev/' | awk '{ print $6 "_:_" $5 "-:-" $4 }')
 do
 
         part=$(echo "$line" | awk -F"_:_" '{ print $1 }')
-        part_usage=$(echo "$line" | awk -F"_:_" '{ print $2 }' | cut -d'%' -f1 )
+        part_usage_percent=$(echo "$line" | awk -F"_:_" '{ print $2 }' | cut -d'%' -f1 )
+		part_usage_byte=$(echo "$line" | awk -F"-:-" '{ print $2 }' )
 
-        if [ $part_usage -ge $THRESHOLD -a -z "$EMAIL" ];
+        if [ $part_usage_percent -ge $THRESHOLDPERCENT -a -z "$EMAIL" ] && [ $part_usage_byte -ge $THRESHOLDBYTE -a -z "$EMAIL" ];
         then
                 EMAIL="$(date): $HOSTNAME sunucusunda disk alani dolmak uzeredir.\n"
-                EMAIL="$EMAIL\n$part ($part_usage%) >= (Threshold = $THRESHOLD%)"
+                EMAIL="$EMAIL\n$part ($part_usage_percent%) >= (Threshold = $THRESHOLDPERCENT%)\n"
+				EMAIL="$EMAIL\n$part ($part_usage_byte%) >= (Threshold = $THRESHOLDBYTE%)"
 
-        elif [ $part_usage -ge $THRESHOLD ];
+        elif [ $part_usage_percent -ge $THRESHOLDPERCENT ] && [ $part_usage_byte -ge $THRESHOLDBYTE ];
         then
-                EMAIL="$EMAIL\n$part ($part_usage%) >= (Threshold = $THRESHOLD%)"
+                EMAIL="$EMAIL\n$part ($part_usage_percent%) >= (Threshold = $THRESHOLDPERCENT%)"
+				EMAIL="$EMAIL\n$part ($part_usage_byte%) >= (Threshold = $THRESHOLDBYTE%)"
+
         fi
 done 
 
