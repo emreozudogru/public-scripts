92df75bf4feac84b31c65809ca7278c943d7d483
Emre Ozudogru <emre@ozudogru.com>
2020-01-14 19:47:37 +0000

    Update install_zabbix-agent.sh

--- install_zabbix-agent.sh
+++ install_zabbix-agent.sh
@@ -43,17 +43,7 @@ echo Ver: $VER
 case $OS in
     CentOS)
     case $VER in
-    6.0)
-    6.1)
-    6.2)
-    6.3)
-    6.4)
-    6.5)
-    6.6)
-    6.7)
-    6.8)
-    6.9)
-    6.10)
+    [6.01-6.99])
     echo CentOS 6
     ;;
 ;;
